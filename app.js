4a576bdb09f14a4e0ffe422be4a50918d1645df3
wuzhangpeng <wzhangpeng1989@163.com>
2017-12-12 21:13:37 +0800

    add

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var swig = require('swig');
+var mongoose = require('mongoose')
+var bodyParser = require('body-parser')
+var Cookies = require('cookies')
+var app = express();
+
+
+app.use('/public', express.static(__dirname + '/public'))
+app.use('/static', express.static(__dirname + '/static'))
+
+
+app.get('/', function (req, res, next) {
+    res.send('<h1>欢迎光临我的博客</h1>')
+});
+
+
+app.listen(8081);
